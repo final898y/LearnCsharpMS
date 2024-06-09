2a3a6623f49c51f55c8a70e493263e3d9c0f4c83
BuildTools <unconfigured@null.spigotmc.org>
2024-06-10 00:56:12 +0800

    TEST

--- 00_test/Program.cs
+++ 00_test/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-//string prefix = Convert.ToChar("").ToString();
+﻿//string prefix = Convert.ToChar("").ToString();
 //Console.WriteLine(prefix);
 
 /*
@@ -21,12 +19,127 @@ dynamic dyVar2 = 5;
 dyVar2 = "c#";
 string s = dyVar2;
 Console.WriteLine($"{s.GetType()}");
-*/
 
-int[] nums = [0,2,1,5,3,4];
-int[] ans = new int[nums.Length];
 
-for(int i=0 ;i<nums.Length+1;i++)
+int n=15;
+              int[] nums = new int[n+1];
+for(int i = 0; i<n+1;i++)
 {
+    if(i<2)
+    {
+        nums[i]=i;
+    }
+    else  
+    {
+        if(i%2==0)
+        {
+            nums[i]=nums[i/2];
+        }
+        else
+        {
+            nums[i]=nums[(i-1)/2]+nums[((i-1)/2)+1];
+        }
+    }
+}
 
+return(nums.Max());
+
+
+/*
+    public int GetMaximumGenerated(int n) {
+        int[] nums = new int[n+1];
+        for(int i = 0; i<n+1;i++)
+        {
+            if(i<2)
+            {
+                nums[i]=i;
+            }
+            else  
+            {
+                if(i%2==0)
+                {
+                    nums[i]=nums[i/2];
+                }
+                else
+                {
+                    nums[i]=nums[(i-1)/2]+nums[(i-1)/2+1];
+                }
+            }
+        }
+
+        if(n==0)
+        {
+            return(0);
+        }
+        else if (n%2==0)
+        {
+            int max = nums[n-1];
+            return(max);
+        } 
+        else 
+        {
+            int max = nums[n];
+            return(max);
+        }
+        
+    }
+}
+
+*/
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums = [3,2,4];
+        int target = 6;
+        //foreach (var item in nums )
+        //{
+        //    Console.WriteLine($"   [{i++}]:  {item}");
+        //}
+        int i = 0;
+        int j = 1;
+        int x = 0;
+        while (i<nums.Length-1)
+            {
+                
+                while(j<nums.Length-i)
+                { 
+                    x  = nums[i]+nums[i+j];
+                    Console.WriteLine($"while_{x}");
+                    if(x!=target)
+                    {
+                       
+                        Console.WriteLine($"while_if_{x}");
+                        Console.WriteLine(j);
+                        j++;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"bk1_{i}{j}");
+                        break;
+                    }
+                }
+                if(x==target)
+                {
+                    Console.WriteLine($"bk2_{i}{j}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"bk3_{i}{j}");
+                    i++;
+                    j = 1;
+                    
+                }
+
+            }
+        Console.WriteLine($"end{i}");
+        Console.WriteLine($"end{j}");
+        Console.WriteLine($"end{x}");
+            
+    }
 }
--- 06_ProjectForeachIfArray/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ 06_ProjectForeachIfArray/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7553a50799671f5928e413726b5e46fd2cf3b20f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714a5d5fad1900c9af3b7bfbe4063ef4715984be")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
